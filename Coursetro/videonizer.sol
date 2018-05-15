7459af30ac34599f5c422329ed8662aece0b9234
AlejoCode <37590339+AlejoCode@users.noreply.github.com>
2018-05-14 19:05:39 -0500

    Version 1.2

--- Coursetro/videonizer.sol
+++ Coursetro/videonizer.sol
@@ -3,15 +3,16 @@ pragma solidity ^0.4.18;
 contract Videonizer {
 
     address owner;
+    uint unActiveStudents = 0;
     uint activeStudents = 0;
     uint percentage = 0;
     uint constant videoPrice = 20 ether;
     
-    function Videonizer() {
+    constructor() public {
         owner = msg.sender;
     }
     
-    function Payable() payable {
+    function Payable() payable public {
     }
 
     modifier onlyOwner {
@@ -19,15 +20,21 @@ contract Videonizer {
         _;
     }
     
-    //modifier haveMoney {
-     //  require(msg.value >= videoPrice);
-     //  _;
-  //}
+    modifier haveMoney {
+        require (msg.value == videoPrice);
+        _;
+    }
+    
+    modifier isStudent {
+        require (students[msg.sender].status);
+            _;
+    }
     
     struct Student {
         address sAddress;
         string fName;
         string lName;
+        bool status;
         bool haveVideo;
         uint tokens;
     }
@@ -35,22 +42,28 @@ contract Videonizer {
     mapping (address => Student) students;
     address[] public studentsAccounts;
 
-    function setStudent(address _address, string _fName, string _lName) public payable {
+    function setStudent(string _fName, string _lName) public {
         
-        if (msg.value != videoPrice) {
-            throw;
-        }
+//        var student = students[_address];
+        students[msg.sender] = Student ({
+            sAddress: msg.sender,
+            fName: _fName,
+            lName: _lName,
+            haveVideo: false,
+            status: false,
+            tokens: 0
+        });
         
-        var student = students[_address];
+    }
+    
+    function buyVideo() haveMoney public payable {
         
-        student.sAddress = _address;
-        student.fName = _fName;
-        student.lName = _lName;
-        student.haveVideo = true;
-        student.tokens += 1;
-
-        studentsAccounts.push(_address) -1;
+        students[msg.sender].tokens += 1;
+        students[msg.sender].haveVideo = true;
+        students[msg.sender].status = true;
+        studentsAccounts.push(msg.sender) -1;
         activeStudents += 1;
+        
     }
     
     function getActiveStudents() view public returns (uint) {
@@ -69,14 +82,14 @@ contract Videonizer {
             students[_address].tokens);
     }
 
-    function PayContract() onlyOwner payable {
+    function PayContract() onlyOwner payable public {
         percentage = (((address(this).balance) / (studentsAccounts.length)));
         for(uint x = 0 ; x < studentsAccounts.length; x++) {
             studentsAccounts[x].transfer(percentage);
         }
     }
     
-    function ContractBalance() view returns (uint) {
-        return this.balance;
+    function ContractBalance() view public returns (uint) {
+        return address(this).balance;
     }
 }
